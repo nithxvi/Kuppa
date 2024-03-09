a94c6f5d8599ae504aa2c8cf6431c5eb3c0e3170
Narenselvam <126243407+Narenselvam@users.noreply.github.com>
2024-03-09 16:02:50 +0530

    relative path for model

--- APP/views.py
+++ APP/views.py
@@ -74,8 +74,8 @@ def Per_Info_8(request):
         form = UserPersonalForm(request.POST)    
         return render(request, '8_Per_Info.html', {'form':form})
     
-model = tensorflow.keras.models.load_model('D:/Project/Kuppa/APP/DISEASE.h5')
-dataset1 = "D:/Project/Kuppa/APP/mod_dataset.csv"
+model = tensorflow.keras.models.load_model('APP\DISEASE.h5')
+dataset1 = "APP\mod_dataset.csv"
 
 def Deploy_9(request): 
     if request.method == "POST":
